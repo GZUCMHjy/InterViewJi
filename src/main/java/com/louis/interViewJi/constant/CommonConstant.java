03af8262a8c6e41115cae357f754e4662bfa1f6c
14754856065hjy <3506428503@qq.com>
2024-10-21 19:42:58 +0800

    初始化面试基

---
+++ src/main/java/com/louis/interViewJi/constant/CommonConstant.java
@@ -0,0 +1,21 @@
+package com.louis.interViewJi.constant;
+
+/**
+ * 通用常量
+ *
+ * @author <a href="https://github.com/lilouis">程序员鱼皮</a>
+ * @from <a href="https://louis.icu">编程导航知识星球</a>
+ */
+public interface CommonConstant {
+
+    /**
+     * 升序
+     */
+    String SORT_ORDER_ASC = "ascend";
+
+    /**
+     * 降序
+     */
+    String SORT_ORDER_DESC = " descend";
+    
+}
---
+++ src/main/java/com/louis/interViewJi/wxmp/WxMpConstant.java
@@ -0,0 +1,16 @@
+package com.louis.interViewJi.wxmp;
+
+/**
+ * 微信公众号相关常量
+ *
+ * @author <a href="https://github.com/lilouis">程序员鱼皮</a>
+ * @from <a href="https://louis.icu">编程导航知识星球</a>
+ **/
+public class WxMpConstant {
+
+    /**
+     * 点击菜单 key
+     */
+    public static final String CLICK_MENU_KEY = "CLICK_MENU_KEY";
+
+}
